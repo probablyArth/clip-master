e728a67f187a7206c6aca4025ced85bb137e996f
Arth <probablyarth@gmail.com>
2024-07-14 02:51:14 +0530

    prisma migration

---
+++ prisma/migrations/20240713212107_cascade_video_on_user_deletion/migration.sql
@@ -0,0 +1,17 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Videos" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "size" BIGINT NOT NULL,
+    "path" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Videos_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Videos" ("id", "name", "path", "size", "userId") SELECT "id", "name", "path", "size", "userId" FROM "Videos";
+DROP TABLE "Videos";
+ALTER TABLE "new_Videos" RENAME TO "Videos";
+CREATE UNIQUE INDEX "Videos_path_key" ON "Videos"("path");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
