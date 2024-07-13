6dd462bc163c24ff63bdd80e9ac13288813d9b9d
Arth <probablyarth@gmail.com>
2024-07-14 03:46:01 +0530

    update error middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,8 +1,12 @@
 import { CustomError } from 'errors/custom-error';
 import { NextFunction, Request, Response } from 'express';
+import { MulterError } from 'multer';
 
 const error = () => {
-  return (err: CustomError, _: Request, res: Response, __: NextFunction) => {
+  return (err: CustomError | MulterError, _: Request, res: Response, __: NextFunction) => {
+    if (err instanceof MulterError) {
+      return res.status(400).json({ code: 400, reasons: [{ message: err.message }] });
+    }
     return res.status(err.statusCode).json({ code: err.statusCode, reasons: err.serializeErrors() });
   };
 };
